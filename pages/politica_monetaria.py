19f74d860a28dd6aec478b62b6854c7d7c32b24c
jguih2376 <jguih@live.com>
2025-03-17 09:58:06 -0300

    otimizando_polit_monetaria

--- pages/politica_monetaria.py
+++ pages/politica_monetaria.py
@@ -4,21 +4,27 @@ from bcb import sgs
 
 @st.cache_data
 def get_data():
-    dolar = sgs.get({'Dólar': 10813}, start='2000-01-01')
+    start_date = '2010-01-01'  # Reduzindo o período
+    dolar = sgs.get({'Dólar': 10813}, start=start_date)
+    selic = sgs.get({'Selic': 432}, start=start_date)
+    ipca = sgs.get({'IPCA': 13522}, start=start_date)
+    
     dolar_atual = dolar.iloc[-1].values[0]
-    selic = sgs.get({'Selic': 432}, start='2000-01-01')
     selic_atual = selic.iloc[-1].values[0]
-    ipca = sgs.get({'IPCA': 13522}, start='2000-01-01')
     ipca_atual = ipca.iloc[-1].values[0]
-    
-
-    
-    juros_real = ()
-    # Calcular juros real
-    
-
+    juros_real = (((1 + selic_atual/100) / (1 + ipca_atual/100)) - 1) * 100
     
     return selic, selic_atual, ipca, ipca_atual, juros_real, dolar, dolar_atual
+
+@st.cache_resource
+def create_chart(data, atual, title, yaxis_title, unit):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data.iloc[:, 0], mode='lines'))
+    fig.add_trace(go.Scatter(x=[data.index[-1]], y=[atual], mode='markers', marker=dict(color='red', size=5)))
+    fig.update_layout(title=title, yaxis_title=yaxis_title, showlegend=False, height=400)
+    fig.add_annotation(x=data.index[-1], y=atual, text=f'{atual:.2f}{unit}', showarrow=True, ay=-40)
+    return fig
+
 
 def app():
     st.title("🏛️Estatística Monetária")
@@ -28,65 +34,9 @@ def app():
 
     col1, col2 = st.columns([5, 1])
     with col1:
-        # Criando gráfico interativo da Selic
-        fig_selic = go.Figure()
-        fig_selic.add_trace(go.Scatter(x=selic.index, y=selic['Selic'], mode='lines'))
-        fig_selic.add_trace(go.Scatter(x=[selic.index[-1]], y=[selic_atual], mode='markers', marker=dict(color='red', size=5)))
-
-        fig_selic.update_layout(
-            title='Taxa de Juros SELIC',
-            title_x=0.4, 
-            yaxis_title='Taxa de Juros (%)',
-
-            showlegend=False,
-            plot_bgcolor='rgba(211, 211, 211, 0.15)'  # Cor de fundo cinza claro
-        )
-        fig_selic.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot', zeroline=False,  range=[0, fig_selic.data[0]['y'].max() * 1.1])
-        fig_selic.update_xaxes(showgrid=False, zeroline=False)
-
-        # Adicionando anotação para destacar o valor atual
-        fig_selic.add_annotation(
-            x=selic.index[-1], 
-            y=selic_atual,
-            text=f'{selic_atual:.2f}%',
-            showarrow=True,
-            arrowhead=0,
-            ax=20,
-            ay=-40,
-            #bgcolor='rgba(255, 255, 255, 0)',
-            bordercolor='yellow'
-        )
-        # Criando gráfico interativo do IPCA
-        fig_ipca = go.Figure()
-        fig_ipca.add_trace(go.Scatter(x=ipca.index, y=ipca['IPCA'], mode='lines'))
-        fig_ipca.add_trace(go.Scatter(x=[ipca.index[-1]], y=[ipca_atual], mode='markers', marker=dict(color='red', size=5)))
-
-        fig_ipca.update_layout(
-            title='IPCA Acumulado 12M',
-            title_x=0.4, 
-            yaxis_title='IPCA acumulado (%)',
-            showlegend=False,
-            plot_bgcolor='rgba(211, 211, 211, 0.15)'  # Cor de fundo cinza claro
-        )
-        fig_ipca.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot',zeroline=False, range=[0, fig_ipca.data[0]['y'].max() * 1.1])
-        fig_ipca.update_xaxes(showgrid=False, zeroline=False)
-
-        # Adicionando anotação para destacar o valor atual
-        fig_ipca.add_annotation(
-            x=ipca.index[-1], 
-            y=ipca_atual,
-            text=f'{ipca_atual:.2f}%',
-            showarrow=True,
-            arrowhead=0,
-            ax=20,
-            ay=-40,
-            #bgcolor='rgba(255, 255, 255, 0)',
-            bordercolor='yellow'
-        )
-
-        # Exibindo os gráficos com o Streamlit
-        st.plotly_chart(fig_selic)
-        st.plotly_chart(fig_ipca)
+        st.plotly_chart(create_chart(selic, selic_atual, 'Taxa de Juros SELIC', 'Taxa de Juros (%)', '%'))
+        st.plotly_chart(create_chart(ipca, ipca_atual, 'IPCA Acumulado 12M', 'IPCA acumulado (%)', '%'))
+        st.plotly_chart(create_chart(dolar, dolar_atual, 'Cotação do Dólar', 'Valor em R$', 'R$'))
 
     with col2:
         st.write('')
@@ -110,7 +60,7 @@ def app():
 
         st.components.v1.html(iframe_code, height=180)
 
-        juros_real = (((1 + selic_atual/100) / (1 + ipca_atual/100)) - 1) * 100
+        
 
         # Exibir o resultado com alinhamento centralizado
         st.write(
@@ -124,23 +74,3 @@ def app():
 )
 
 
-    with col1:
-        # Criando gráfico interativo do Dólar
-        fig_dolar = go.Figure()
-
-        fig_dolar.add_trace(go.Scatter(x=dolar.index, y=dolar['Dólar'], mode='lines', line=dict(width=1), name="Cotação do Dólar"))
-
-        fig_dolar.add_trace(go.Scatter(x=[dolar.index[-1]], y=[dolar_atual], mode='markers', marker=dict(color='red', size=5), name="Última cotação"))
-
-        fig_dolar.update_layout(title='Cotação do Dólar', title_x=0.4, yaxis_title='Valor em R$', showlegend=False, 
-                                plot_bgcolor='rgba(211, 211, 211, 0.10)', height=550)
-        
-        fig_dolar.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot', zeroline=False,
-                                range=[dolar['Dólar'].min() * 0.9, dolar['Dólar'].max() * 1.1], tickformat='.2f')
-
-        fig_dolar.update_xaxes(showgrid=False, zeroline=False)
-
-        fig_dolar.add_annotation(x=dolar.index[-1], y=dolar_atual, text=f'R${dolar_atual:.2f}', showarrow=True,
-                                arrowhead=0, ax=20, ay=-40, bordercolor='yellow')
-        
-        st.plotly_chart(fig_dolar)
